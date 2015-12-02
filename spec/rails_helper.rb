81356b4d0e68d3d178a955cc5201438ac43214a9
hoangphanea <hoang.phan@eastagile.com>
2015-12-02 09:55:31 +0700

    [b] Fix config load order

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
