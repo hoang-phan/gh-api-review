07c1af29f1d626eb77ac759ddded0b3fb1a39923
hoangphanea <hoang.phan@eastagile.com>
2015-12-09 12:18:57 +0700

    [b] Fix null committer name

--- features/step_definitions/commit_steps.rb
+++ features/step_definitions/commit_steps.rb
@@ -37,9 +37,22 @@ Given(/^I have some commits of repository '(.*)' on Github$/) do |repo|
       'author' => {
         'date' => Date.today
       },
+      'message' => 'message'
+    },
+    'committer' => {
+      'login' => 'committer'
+    }
+  }
+
+  @commits_result2 = {
+    'sha' => '88383',
+    'commit' => {
+      'author' => {
+        'date' => Date.today
+      },
       'message' => 'message',
       'committer' => {
-        'login' => 'committer'
+        'name' => 'committer2'
       }
     }
   }
@@ -49,13 +62,14 @@ Given(/^I have some commits of repository '(.*)' on Github$/) do |repo|
   allow(@client).to receive(:commit).with(repo, anything, per_page: GITHUB_ENV['results_per_page']).and_return(@commit_json)
   allow(@client).to receive(:branches).with(repo, page: 1, per_page: GITHUB_ENV['results_per_page']).and_return(@branches_json)
   allow(@client).to receive(:branches).with(repo, page: 2, per_page: GITHUB_ENV['results_per_page'])
-  allow(@client).to receive(:commits_since).with(repo, anything, anything).and_return([@commits_result])
+  allow(@client).to receive(:commits_since).with(repo, anything, anything).and_return([@commits_result, @commits_result2])
   $client = @client
 end
 
 Then(/^the commits of repository '(.*)' should be reloaded$/) do |repo|
   repository = Repository.find_by(full_name: repo)
-  commit = repository.commits.find_by(sha: @commits_result['sha'], message: @commits_result['commit']['message'], committer: @commits_result['commit']['committer']['login'])
+  commit = repository.commits.find_by(sha: @commits_result['sha'], message: @commits_result['commit']['message'], committer: @commits_result['committer']['login'])
   expect(commit).to be_present
-  expect(commit.file_changes.pluck(:patch)).to match_array ["\n...\n<span>   52</span> | <span>   52</span> | <span class=\"unchanged-line\"> gem &#39;redis-rails&#39;</span>\n<span>   53</span> | <span>   53</span> | <span class=\"unchanged-line\"> </span>\n<span>   54</span> | <span>   54</span> | <span class=\"unchanged-line\"> gem &#39;stripe&#39;</span>\n<span>   55</span> | <span>   55</span> | <span class=\"unchanged-line\"> </span>\n<span>     </span> | <span>   56</span> | <span class=\"added-line\">+gem &#39;twilio-ruby&#39;</span>\n<span>     </span> | <span>   57</span> | <span class=\"added-line\">+</span>\n<span>   56</span> | <span>   58</span> | <span class=\"unchanged-line\"> group :development, :test do</span>\n<span>   57</span> | <span>   59</span> | <span class=\"unchanged-line\">   gem &#39;byebug&#39;, &#39;~&gt; 8.0.0&#39;</span>\n<span>   58</span> | <span>   60</span> | <span class=\"unchanged-line\">   gem &#39;rspec-rails&#39;, &#39;~&gt; 3.3.3&#39;</span>", "\n...\n<span>   10</span> | <span>   10</span> | <span class=\"unchanged-line\"></span>\n<span>   11</span> | <span>   11</span> | <span class=\"unchanged-line\">   it { should validate_presence_of(:telephone_number) }</span>\n<span>   12</span> | <span>   12</span> | <span class=\"unchanged-line\">   it { should validate_presence_of(:country) }</span>\n<span>   13</span> | <span>   13</span> | <span class=\"unchanged-line\">   it { should validate_presence_of(:postcode) }</span>\n<span>     </span> | <span>   14</span> | <span class=\"added-line\">+</span>\n<span>   14</span> | <span>   15</span> | <span class=\"unchanged-line\"> end</span>"]
+  expect(commit.file_changes.pluck(:patch)).to include "\n...\n<span>   52</span> | <span>   52</span> | <span class=\"unchanged-line\"> gem &#39;redis-rails&#39;</span>\n<span>   53</span> | <span>   53</span> | <span class=\"unchanged-line\"> </span>\n<span>   54</span> | <span>   54</span> | <span class=\"unchanged-line\"> gem &#39;stripe&#39;</span>\n<span>   55</span> | <span>   55</span> | <span class=\"unchanged-line\"> </span>\n<span>     </span> | <span>   56</span> | <span class=\"added-line\">+gem &#39;twilio-ruby&#39;</span>\n<span>     </span> | <span>   57</span> | <span class=\"added-line\">+</span>\n<span>   56</span> | <span>   58</span> | <span class=\"unchanged-line\"> group :development, :test do</span>\n<span>   57</span> | <span>   59</span> | <span class=\"unchanged-line\">   gem &#39;byebug&#39;, &#39;~&gt; 8.0.0&#39;</span>\n<span>   58</span> | <span>   60</span> | <span class=\"unchanged-line\">   gem &#39;rspec-rails&#39;, &#39;~&gt; 3.3.3&#39;</span>", "\n...\n<span>   10</span> | <span>   10</span> | <span class=\"unchanged-line\"></span>\n<span>   11</span> | <span>   11</span> | <span class=\"unchanged-line\">   it { should validate_presence_of(:telephone_number) }</span>\n<span>   12</span> | <span>   12</span> | <span class=\"unchanged-line\">   it { should validate_presence_of(:country) }</span>\n<span>   13</span> | <span>   13</span> | <span class=\"unchanged-line\">   it { should validate_presence_of(:postcode) }</span>\n<span>     </span> | <span>   14</span> | <span class=\"added-line\">+</span>\n<span>   14</span> | <span>   15</span> | <span class=\"unchanged-line\"> end</span>"
+  expect(repository.commits).to be_exists sha: @commits_result2['sha'], message: @commits_result2['commit']['message'], committer: @commits_result2['commit']['committer']['name']
 end
--- spec/workers/repository_commits_fetch_spec.rb
+++ spec/workers/repository_commits_fetch_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe RepositoryCommitsFetch do
-  COMMIT_ATTRIBUTES = %w(sha committer committed_at message)
-
   describe '#perform' do
-    let(:watched_json) do
+    let(:watched_json1) do
       {
         'sha' => sha,
         'commit' => {
           'author' => {
             'date' => committed_at
           },
+          'message' => message
+        },
+        'committer' => {
+          'login' => committer
+        }
+      }
+    end
+
+    let(:watched_json2) do
+      {
+        'sha' => sha1,
+        'commit' => {
+          'author' => {
+            'date' => committed_at
+          },
           'message' => message,
           'committer' => {
-            'login' => committer
+            'name' => committer_name
           }
         }
       }
@@ -28,7 +41,7 @@ RSpec.describe RepositoryCommitsFetch do
           },
           'message' => message,
           'committer' => {
-            'login' => 'new commiter'
+            'name' => 'new committer'
           }
         }
       }
@@ -43,7 +56,7 @@ RSpec.describe RepositoryCommitsFetch do
           },
           'message' => 'another message',
           'committer' => {
-            'login' => 'anotheruser'
+            'name' => 'anotheruser'
           }
         }
       }
@@ -54,9 +67,10 @@ RSpec.describe RepositoryCommitsFetch do
     let!(:unwatch_branch) { create(:branch, repository: repository, watched: false, name: branches.first['name']) }
     let(:repository) { create(:repository, watched: true) }
     let(:sha) { Faker::Number.hexadecimal(8) }
+    let(:sha1) { Faker::Number.hexadecimal(8) }
     let(:committer) { Faker::Internet.user_name }
+    let(:committer_name) { Faker::Name.name }
     let(:committed_at) { DateTime.parse('2015-10-10') }
-    let(:new_commit) { Commit.last }
     let(:message) { Faker::Lorem.sentence }
     let(:existing_sha) { 'existing_sha' }
     let!(:existing_commit) { create(:commit, sha: existing_sha) }
@@ -64,7 +78,7 @@ RSpec.describe RepositoryCommitsFetch do
     before do
       allow(fake_client).to receive(:branches).with(repository.full_name, page: 1, per_page: GITHUB_ENV['results_per_page']).and_return(branches)
       allow(fake_client).to receive(:branches).with(repository.full_name, page: 2, per_page: GITHUB_ENV['results_per_page'])
-      allow(fake_client).to receive(:commits_since).with(repository.full_name, anything, branches.second['name']).and_return([watched_json])
+      allow(fake_client).to receive(:commits_since).with(repository.full_name, anything, branches.second['name']).and_return([watched_json1, watched_json2])
       allow(fake_client).to receive(:commits_since).with(repository.full_name, anything, unwatch_branch.name).and_return([unwatched_json])
       $client = fake_client
     end
@@ -72,11 +86,10 @@ RSpec.describe RepositoryCommitsFetch do
     it 'fetches new commits of the non-unwatched branches' do
       expect {
         subject.perform(repository.id)
-      }.to change(repository.commits, :count).by(1)
+      }.to change(repository.commits, :count).by(2)
 
-      COMMIT_ATTRIBUTES.each do |key|
-        expect(new_commit.send(key)).to eq send(key)
-      end
+      expect(repository.commits).to be_exists(sha: sha, committer: committer, message: message, committed_at: committed_at)
+      expect(repository.commits).to be_exists(sha: sha1, committer: committer_name, message: message, committed_at: committed_at)
     end
   end
 end
