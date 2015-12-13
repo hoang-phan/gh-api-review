908408025d78aac38b069de8fd195d818a9c3276
Hoang Phan <hoangphanbk10@gmail.com>
2015-12-13 03:42:08 -0800

    [r] Move logic from view to controller for testing

--- spec/helpers/commit_helper_spec.rb
+++ spec/helpers/commit_helper_spec.rb
@@ -19,4 +19,107 @@ RSpec.describe CommitHelper, type: :helper do
       it { is_expected.to eq "<p><span class=\"line-number\">#{key.rjust(5)}</span><span class=\"line #{special_class}\">#{line}</span></p>" }
     end
   end
+
+  describe '#show_comment?' do
+    subject { helper.show_comment?(is_modified, klass) }
+
+    let(:is_modified) { false }
+    let(:klass) { 'removed-line' }
+
+    context 'is modified' do
+      let(:is_modified) { true }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'class is added-line' do
+      let(:klass) { 'added-line' }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'otherwise' do
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe '#display_comment' do
+    let(:line_comments) do
+      {
+        filename => {
+          line => body
+        }
+      }
+    end
+
+    let(:result) { helper.display_comment(line_comments, filename, line, is_modified, klass) }
+    let(:is_modified) { false }
+    let(:klass) { 'class-name' }
+    let(:filename) { 'dir/filename.ext' }
+    let(:line) { 3 }
+    let(:body) { 'my comment body' }
+    let(:show_comment) { true }
+
+    before do
+      expect(helper).to receive(:show_comment?).with(is_modified, klass).and_return(show_comment)
+    end
+
+    context 'comment not shown' do
+      let(:show_comment) { false }
+      
+      it 'returns nil' do
+        expect(helper).not_to receive(:render)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'comment not exists' do
+      let(:body) { nil }
+
+      it 'returns nil' do
+        expect(helper).not_to receive(:render)
+        expect(result).to be_nil
+      end
+    end
+
+    context 'comment exists' do
+      let(:partial) { 'My partial' }
+
+      it 'renders comment partial' do
+        expect(helper).to receive(:render).with('file_changes/comment', comment: body).and_return(partial)
+        expect(result).to eq partial
+      end
+    end
+  end
+
+  describe '#render_line_changes' do
+    subject { render_line_changes(line_changes, klass, filename) }
+
+    let(:line_changes) do
+      {
+        key1 => value1,
+        key2 => value2
+      }
+    end
+    let(:key1) { '1' }
+    let(:key2) { '5' }
+    let(:value1) { 'value1' }
+    let(:value2) { 'value2' }
+    let(:partial1) { 'partial1' }
+    let(:partial2) { 'partial2' }
+
+    let(:klass) { 'class' }
+    let(:filename) { 'dir/filename' }
+    
+    before do
+      expect(helper).to receive(:render)
+                          .with('file_changes/line_change', key: key1, value: value1, special_class: klass, filename: filename)
+                          .and_return(partial1)
+      expect(helper).to receive(:render)
+                          .with('file_changes/line_change', key: key2, value: value2, special_class: klass, filename: filename)
+                          .and_return(partial2)
+    end
+
+    it { is_expected.to eq "#{partial1}#{partial2}" }
+  end
 end
