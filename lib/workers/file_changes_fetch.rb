0e87cdd8947f0af7a8ccb696830fb830f20aa624
Hoang Phan <hoangphanbk10@gmail.com>
2015-12-13 16:53:34 -0800

    [r] Do not display binary changes

--- lib/workers/file_changes_fetch.rb
+++ lib/workers/file_changes_fetch.rb
@@ -8,7 +8,9 @@ class FileChangesFetch
       FileChange.where(commit: commit).delete_all
 
       files = $client.commit(commit.repository.full_name, commit.sha, per_page: GITHUB_ENV['results_per_page'])['files']
-
+      files = files.select do |file|
+        file['patch'].present?
+      end
       FileChange.import(files.map do |file|
         FileChange.new(filename: file['filename'], patch: file['patch'], line_changes: PatchHelper.build_patch(file['patch']), commit_id: commit.id)
       end)
