ffe641b884ba1539696819249f614df52fed3830
hoangphanea <hoang.phan@eastagile.com>
2015-12-22 15:47:45 +0700

    [r] Relocate and refactor analyzing logic

--- lib/workers/analyzer.rb
+++ lib/workers/analyzer.rb
@@ -2,25 +2,6 @@ class Analyzer
   include Sidekiq::Worker
 
   def perform(commit_id)
-    if commit = Commit.find_by_id(commit_id)
-      commit.file_changes.find_each do |file|
-        file_type = file.filename.split('.').last
-        suggestions = {}
-        file.line_changes.each do |chunk|
-          chunk['+'].each do |ln, values|
-            if values[0].present?
-              LINE_RULES.each do |rule|
-                regexes = rule['regex']
-                regex = regexes[file_type] || regexes['all']
-                if regex.present? && values[0].match(regex)
-                  suggestions[(ln.to_i - rule['offset'].to_i).to_s] = (suggestions[ln] || []) << rule['name']
-                end
-              end
-            end
-          end
-        end
-        file.update(suggestions: suggestions)
-      end
-    end
+    FileChange.where(commit_id: commit_id).find_each(&:analyze)
   end
 end
--- spec/models/file_change_spec.rb
+++ spec/models/file_change_spec.rb
@@ -2,4 +2,82 @@ require 'rails_helper'
 
 RSpec.describe FileChange, type: :model do
   it { is_expected.to belong_to :commit }
+
+  describe '#analyze' do
+    subject { build(:file_change, line_changes: line_changes, filename: 'sample.rb') }
+
+    let(:line_changes) do
+      [
+        {
+          '+' => {
+          line_1 => [value_1],
+          line_2 => [value_2],
+          line_3 => [value_3]
+          }
+        },
+        {
+          '+' => {
+            line_4 => [value_4],
+            line_5 => [value_5]
+          }
+        }
+      ]
+    end
+
+    let(:rules) do
+      [
+        {
+          'regex' => {
+            'rb' => 'matching 1'
+          },
+          'name' => rule1_name
+        },
+        {
+          'regex' => {
+            'all' => 'matching 2'
+          },
+          'name' => rule2_name
+        },
+        {
+          'regex' => {
+            'all' => 'matching 3'
+          },
+          'name' => rule3_name,
+          'offset' => 1
+        }
+      ]
+    end
+
+    let(:expected_result) do
+      {
+        line_1 => [rule1_name],
+        line_2 => [rule2_name],
+        line_3 => [rule1_name, rule2_name],
+        (line_5.to_i - 1).to_s => [rule3_name]
+      }
+    end
+
+    let(:line_1) { '5' }
+    let(:line_2) { '10' }
+    let(:line_3) { '11' }
+    let(:line_4) { '13' }
+    let(:line_5) { '17' }
+    let(:value_1) { 'matching 1 and sth' }
+    let(:value_2) { 'matching 2' }
+    let(:value_3) { 'matching 1 and matching 2' }
+    let(:value_4) { 'nomatch' }
+    let(:value_5) { 'matching 3' }
+    let(:rule1_name) { Faker::Name.name }
+    let(:rule2_name) { Faker::Name.name }
+    let(:rule3_name) { Faker::Name.name }
+
+    before do
+      stub_const('LINE_RULES', rules)
+      subject.analyze
+    end
+
+    it 'updates suggestions with correct result' do
+      expect(subject.suggestions).to eq expected_result
+    end
+  end
 end
